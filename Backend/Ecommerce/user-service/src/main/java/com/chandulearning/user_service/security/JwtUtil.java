bf9d6207f779a995789f590664cf5c60f1a17a88
Chandra Shekar Mekala <chandrashekar.mekala2001@gmail.com>
2025-07-06 15:23:55 +0530

    SCRUM-35: Added feign client configuraton

--- Backend/Ecommerce/user-service/src/main/java/com/chandulearning/user_service/security/JwtUtil.java
+++ Backend/Ecommerce/user-service/src/main/java/com/chandulearning/user_service/security/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.*;
 //import io.jsonwebtoken.Jwts;
 //import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +15,11 @@ import java.util.Map;
 
 @Component
 public class JwtUtil {
-    private final String SECRET_KEY = "chandu_learning_super_secure_secret_key_123!";
-    private final long EXPIRATION_TIME = 1000 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private  String SECRET_KEY;
+    @Value("${jwt.expiration}")
+    private long EXPIRATION_TIME ;
 
     private Key getSigningKey(){
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
